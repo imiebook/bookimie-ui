322de23e23984a99111a6f2b9f9892887c70d4e6
Gabriel Pied <gabrielpied@gmail.com>
2018-03-06 10:57:39 +0100

    add breadcrum + begin search

--- templates/header.php
+++ templates/header.php
@@ -14,14 +14,20 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="./src/assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="../src/assets/css/style.css">
   </head>
   <body>
       <header>
           <nav class="navbar navbar-dark bg-primary">
               <a class="navbar-brand" href="https://bookimie.fr">
-                <img src="./src/assets/img/logoIMIE.png" height="70" alt="Logo IMIE">
+                <img src="../src/assets/img/logoIMIE.png" height="70" alt="Logo IMIE">
               </a>
           </nav>
+          <nav aria-label="breadcrumb">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="#">Accueil</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Recherche</li>
+              </ol>
+        </nav>
       </header>
       <main>
--- templates/index.php
+++ templates/index.php
@@ -1,15 +1,18 @@
 <?php include("header.php")?>
-<!-- form inscription -->
-<form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+
+  <div class="mx-auto" style="width: 300px; margin-top: 15%;">
+    <!-- form inscription -->
+    <form>
+      <div class="form-group">
+        <label for="exampleInputEmail1">Adresse email IMIE</label>
+        <input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp" placeholder="Email">
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Mot de passe</label>
+        <input type="password" class="form-control" id="InputPassword1" placeholder="Mot de passe">
+      </div>
+      <button type="submit" class="btn btn-primary">Envoyer</button>
+    </form>
+</div>
 
 <?php include("footer.php")?>
