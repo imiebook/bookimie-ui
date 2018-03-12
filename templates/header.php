da58ba3459fe41e06bf0936688437240559ed0da
Gabriel Pied <gabrielpied@gmail.com>
2018-03-12 13:22:46 +0100

    style-n

--- templates/header.php
+++ templates/header.php
@@ -16,6 +16,7 @@
     <link rel="stylesheet" type="text/css" href="/src/assets/css/reset.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="/src/assets/css/style.css">
+	 <link rel="stylesheet" type="text/css" href="/src/assets/css/style-n.css">
   </head>
   <body>
       <header>
