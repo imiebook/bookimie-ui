22d66a5af8c879e89e89e92e143e40b4ff966d72
Gabriel Pied <gabrielpied@gmail.com>
2018-03-12 13:20:45 +0100

    add css

--- templates/index.php
+++ templates/index.php
@@ -1,39 +1,39 @@
 <?php include("header.php")?>
 
-  <div class="mx-auto" style="width: 300px; margin-top: 15%;">
-    
-    <!-- form inscription -->
-    <form class="needs-validation" novalidate>
-      <div class="form-group">
-        <label for="validationCustom01">Adresse email IMIE</label>
-        <div class="input-group">
-          <div class="input-group-prepend">
-            <span class="input-group-text" id="inputGroupPrepend">@</span>
-          </div>
-          <input type="email" class="form-control" id="validationCustom01" placeholder="Adresse e-mail" aria-describedby="inputGroupPrepend" required>
-          <div class="invalid-feedback">
-            Veuillez taper une adresse mail valide.
-          </div>
-        </div>
-        
-        <div class="form-group">
-          <label for="validationCustom02">Mot de passe</label>
-          <input type="password" class="form-control" id="validationCustom02" placeholder="Mot de passe" required>
-            <div class="invalid-feedback">
-              Veuillez saisir un mot de passe.
-            </div>
-        </div>
-        <div class="invalid-feedback-server">
-          Votre e-mail ou mot de passe est incorrect.
-        </div>
+<div class="mx-auto" style="width: 300px; margin-top: 15%;">
 
-        <div class="row justify-content-around">
-          <button type="submit" id="mdpoublie"class="btn btn-primary">Mot de passe oublié ?</button>
-          <button type="submit" class="btn btn-primary">Envoyer</button>
-        </div>
-        
-      </div>
-    </form>
+	<!-- form inscription -->
+	<form class="needs-validation" novalidate>
+		<div class="form-group">
+			<label for="validationCustom01">Adresse email IMIE</label>
+			<div class="input-group">
+				<div class="input-group-prepend">
+					<span class="input-group-text" id="inputGroupPrepend">@</span>
+				</div>
+				<input type="email" class="form-control" id="validationCustom01" placeholder="Adresse e-mail" aria-describedby="inputGroupPrepend" required>
+				<div class="invalid-feedback">
+					Veuillez taper une adresse mail valide.
+				</div>
+			</div>
+
+			<div class="form-group">
+				<label for="validationCustom02">Mot de passe</label>
+				<input type="password" class="form-control" id="validationCustom02" placeholder="Mot de passe" required>
+				<div class="invalid-feedback">
+					Veuillez saisir un mot de passe.
+				</div>
+			</div>
+			<div class="invalid-feedback-server">
+				Votre e-mail ou mot de passe est incorrect.
+			</div>
+
+			<div class="row justify-content-around">
+				<button type="submit" id="mdpoublie"class="btn btn-primary">Mot de passe oublié ?</button>
+				<button type="submit" class="btn btn-primary">Envoyer</button>
+			</div>
+
+		</div>
+	</form>
 </div>
 <script src="doNotSendForm.js"></script>    
 <?php include("footer.php")?>
--- templates/recherche.php
+++ templates/recherche.php
@@ -1,36 +1,23 @@
 <?php include("header.php")?>
-<!-- SIDEBAR
-      <div id="sidebar" class="col-12 col-md-3 col-xl-2 bd-sidebar" style="border: 1px solid black">
-        <div>
-          <ul>
-          <li>
-            1
-          </li>
-          <li>
-            2
-          </li>
-          </ul>
-        </div>
-      </div>
--->
-  <div class="searchResults">
-    <article>
-      Résultat de recherche
-    </article>
-</div>
-    <nav id="pagination" aria-label="Page navigation example">
-      <ul class="pagination justify-content-center">
-        <li class="page-item disabled">
-          <a class="page-link" href="#" tabindex="-1">Previous</a>
-        </li>
-        <li class="page-item"><a class="page-link" href="#">1</a></li>
-        <li class="page-item"><a class="page-link" href="#">2</a></li>
-        <li class="page-item"><a class="page-link" href="#">3</a></li>
-        <li class="page-item">
-          <a class="page-link" href="#">Next</a>
-        </li>
-      </ul>
-    </nav>
+
+<section class="searchResults">
+	<article>
+		Résultat de recherche
+	</article>
+</section>
+<nav id="pagination" aria-label="Page navigation example">
+	<ul class="pagination justify-content-center">
+		<li class="page-item disabled">
+			<a class="page-link" href="#" tabindex="-1">Previous</a>
+		</li>
+		<li class="page-item"><a class="page-link" href="#">1</a></li>
+		<li class="page-item"><a class="page-link" href="#">2</a></li>
+		<li class="page-item"><a class="page-link" href="#">3</a></li>
+		<li class="page-item">
+			<a class="page-link" href="#">Next</a>
+		</li>
+	</ul>
+</nav>
 
 
 <?php include("footer.php")?>
