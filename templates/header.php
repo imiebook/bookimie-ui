5162cc9e88ee839a077bfe5676f3f97f1a8b90f0
Gabriel Pied <gabrielpied@gmail.com>
2018-03-12 09:50:34 +0100

    change relative route

--- templates/header.php
+++ templates/header.php
@@ -13,15 +13,15 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" type="text/css" href="../src/assets/css/reset.css">
+    <link rel="stylesheet" type="text/css" href="/src/assets/css/reset.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="../src/assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="/src/assets/css/style.css">
   </head>
   <body>
       <header>
           <nav class="navbar navbar-dark bg-primary fixed-top">
               <a class="navbar-brand" href="https://bookimie.fr">
-                <img src="../src/assets/img/logoIMIE.png" alt="Logo IMIE">
+                <img src="/src/assets/img/logoIMIE.png" alt="Logo IMIE">
               </a>
           </nav>
           <nav id="breadcrumb" aria-label="breadcrumb">
