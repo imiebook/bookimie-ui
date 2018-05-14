49057f765a45ab4454b4f9059f1c8794ca8433e2
Gabriel Pied <gabrielpied@gmail.com>
2018-05-14 15:59:39 +0200

    add sidebar + filters

--- templates/recherche.php
+++ templates/recherche.php
@@ -1,13 +1,39 @@
 <?php include("header.php")?>
-<div class="row flex-xl-nowrap">
-	<section class="col-12 col-md-3 col-xl-2 bd-sidebar">
-		<nav class="col-md-2 d-none d-md-block bg-light sidebar">
-			ceci est un test de navbar verticale et latérale
-		</nav>
-	</section>
-	</div>
-<section id="searchResults">
-	Résultats de recherche
+<div class="container">
+<div class="row">
+  <div class="col-sm-3">
+    <div class="sidebar-nav">
+        <div class="navbar-header">
+          <span class="visible-xs navbar-brand">FILTRES</span>
+        </div>
+        <div class="navbar-collapse">
+          <div class="nav navbar-nav">
+			  <form class="form-inline">
+					<input class="form-control mr-sm-2" type="search" placeholder="Compétences" aria-label="Search">
+				</form>
+			  <form>
+			  <div class="form-group">
+				<label for="formControlRange">Selectionnez un rayon</label>
+				<input type="range" class="form-control-range" id="formControlRange">
+			  </div>		
+			</form>
+			  <form class="form-inline">
+					<input class="form-control mr-sm-2" type="search" placeholder="Rechercher une entreprise" aria-label="Search">
+				</form>
+            <form>
+			  <div class="form-group">
+				<label for="formControlRange">Expérience</label>
+				<input type="range" class="form-control-range" id="formControlRange">
+			  </div>		
+			</form>
+			  <form class="form-inline">
+					<input class="form-control mr-sm-2" type="search" placeholder="Recherchez une ville" aria-label="Search">
+				</form>
+          </div>
+        </div><!--/.nav-collapse -->
+    </div>
+  </div>
+  <section id="searchResults" class="col-sm-9">
 	<article class="row">
 		<img src="/src/assets/img/gabriel.jpg" alt="Photo de profil Gabriel Pied" class="img-fluid">
 		<div class="col">
@@ -85,7 +111,8 @@
 		</div>
 	</article>
 </section>
-
+  </div>
+</div>
 <nav id="pagination" aria-label="Page navigation example">
 	<ul class="pagination justify-content-center">
 		<li class="page-item disabled">
